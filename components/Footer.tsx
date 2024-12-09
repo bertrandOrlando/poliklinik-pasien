1e7e02fbaf5a3b638ce95673c15da9cf3d5dc2fb
bertrandOrlando <91016925+Frox8@users.noreply.github.com>
2024-12-09 23:49:13 +0700

    feat: add footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="flex w-full items-center justify-center bg-secondaryCol p-4 text-white">
+      Copyright © 2024 Kelompok 2 Rekayasa Perangkat Lunak, All Right Reserved
+    </footer>
+  );
+}
