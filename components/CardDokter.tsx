0d04aeb6ad405edcf07d18368b70be2c8c462fd3
bertrandOrlando <91016925+Frox8@users.noreply.github.com>
2024-12-11 19:59:59 +0700

    fix: filter jadwal dokter

--- components/CardDokter.tsx
+++ components/CardDokter.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { Button, Link } from "@nextui-org/react";
+import { useDayName } from "@/utils/useDayName";
 
 export type CardDokterProps = {
   id_pegawai: number;
@@ -16,6 +17,9 @@ export type CardDokterProps = {
 
 export default function CardDokter(props: CardDokterProps) {
   const dokterUrl = props.id_pegawai + "-" + props.nama.split(" ").join("-");
+
+  const dayName = useDayName();
+  const tomorrow = dayName.getTomorrowDay();
 
   return (
     <div className="flex flex-col justify-between rounded-md px-10 py-6 shadow-md">
@@ -34,13 +38,18 @@ export default function CardDokter(props: CardDokterProps) {
       </div>
       <hr />
       <div className="flex flex-row gap-3 py-3">
-        <span>Jadwal Besok: </span>
-        <div>
-          {props.jadwal.map((item, index) => (
-            <p key={index}>
-              {item.start_time} - {item.end_time}
-            </p>
-          ))}
+        <span>
+          Jam Praktik Besok <span className="font-semibold">({tomorrow})</span>:{" "}
+        </span>
+        <div className="">
+          {props.jadwal.map((item, index) => {
+            if (item.hari.toLowerCase() === tomorrow.toLowerCase())
+              return (
+                <p key={index}>
+                  {item.start_time} - {item.end_time}
+                </p>
+              );
+          })}
         </div>
       </div>
       <Button
